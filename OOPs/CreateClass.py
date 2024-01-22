b74995e2836d5dd9929f0df1f23be4a508bbc944
Vedant <guptavedant1995@yahoo.com>
2024-01-22 00:36:00 -0700

    started OOPs

---
+++ OOPs/CreateClass.py
@@ -0,0 +1,15 @@
+# class Robot():
+#     battery_level = 12
+#     degrees_of_freedom = 25
+
+
+# robot_a = Robot()
+# robot_b = Robot()
+# print(robot_a.battery_level) 
+
+
+class StarCookie():
+    pass
+
+
+star_cookie1 = StarCookie()
