c8607ac072684ae45b4b4cceb51c867e8ce35090
Vedant <guptavedant1995@yahoo.com>
2024-02-13 23:38:06 -0700

    started Tree lecture

---
+++ Trees/CreateTree.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, data, children= []):
+        self.data =data
+        self.children = children
+    
+    def __str__(self, level=0):
+        ret = " "*level + str(self.data) + "\n"
+        for child in self.children:
+            ret = child.__str__(level +1)
+    
+    def addChild(self, TreeNode):
+        self.children.append(TreeNode)
+    
+
+tree = TreeNode('Drinks', [])
+cold = TreeNode('cold', [])
+hot = TreeNode('Hot', [])
+tree.addChild(cold)
+tree.addChild(hot)  
+print(tree)
