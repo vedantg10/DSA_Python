2317fdcbd8659c738645c71b949137e9d648fd04
Vedant <guptavedant1995@yahoo.com>
2024-02-26 23:28:17 -0700

    completed insertion in AVL Tree

--- AVL Tree/InsertionNode.py
+++ AVL Tree/InsertionNode.py
@@ -48,6 +48,54 @@ def levelOrderTraversal(rootNode):
             customQueue.enqueue(root.value.rightChild)
 
 
+def getHeight(rootNode):
+    if not rootNode:
+        return 0
+    return rootNode.height
+
+def rightRotate(disbalanceNode):
+    newRoot = disbalanceNode.leftChild
+    disbalanceNode.leftChild = disbalanceNode.leftChild.rightChild
+    newRoot.rightChild = disbalanceNode
+    disbalanceNode.height = 1+max(getHeight(disbalanceNode.leftChild), getHeight(disbalanceNode.rightChild))
+    return newRoot
+
+def leftRotate(disbalanceNode):
+    newRoot = disbalanceNode.righChild
+    disbalanceNode.rightChild = disbalanceNode.rightChild.leftChild
+    newRoot.leftChild = disbalanceNode
+    disbalanceNode.height = 1+max(getHeight(disbalanceNode.leftChild), getHeight(disbalanceNode.rightChild))
+    return newRoot
+
+def getBalance(rootNode):
+    if not rootNode:
+        return 0
+    return getHeight((rootNode.leftChild) - getHeight(rootNode.rightChild))
+
+def insertNode(rootNode, nodeValue):
+    if not rootNode:
+        return AVLNode(nodeValue)
+    elif nodeValue < rootNode.data:
+        rootNode.leftChild = insertNode(rootNode.leftChild, nodeValue)
+    else:
+        rootNode.rightChild = insertNode(rootNode.rightChild, nodeValue)
+    
+    rootNode.height = 1 +max(getHeight(rootNode.leftChild), getHeight(rootNode.rightChild))
+    balance = getBalance(rootNode)
+    if balance >1 and nodeValue <rootNode.leftChild.data:
+        return rightRotate(rootNode)
+    if balance>1 and nodeValue > rootNode.leftChild.data:
+        rootNode.leftChild = leftRotate(rootNode.leftChild)
+    if balance < -1 and nodeValue > rootNode.rightChild.data:
+        return leftRotate(rootNode)
+    if balance<-1 and nodeValue < rootNode.rightChild.data:
+        rootNode.rightChild = rightRotate(rootNode.rightChild)
+    if balance <-1 and nodeValue < rootNode.rightChild.data:
+        rootNode.rightChild = rightRotate(rootNode.rightChild)
+        return leftRotate(rootNode)
+    return rootNode
+
+
 preOrderTraversal(newAVlTree)
 
 
