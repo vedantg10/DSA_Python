abc6ec63674ee1955aba00c3cd1f9ca540c46acc
Vedant <guptavedant1995@yahoo.com>
2024-02-29 22:34:34 -0700

    started binary heap

---
+++ Binary Heap/Traversal.py
@@ -0,0 +1,30 @@
+class Heap():
+    def __init__(self, size):
+        self.customList = (size+1) *[None]
+        self.heapSize = 0
+        self.maxSize = size+1
+
+
+def peakofHeap(rootNode):
+    if not rootNode:
+        return
+    else:
+        return rootNode.customList[1]
+    
+def sizeofHeap(rootNode):
+    if not rootNode:
+        return
+    else:
+        return rootNode.heapSize
+
+
+def levelOrder(rootNode):
+    if not rootNode:
+        return
+    else:
+        for i in range(1,rootNode.heapSize+1):
+            print(rootNode.customList[i])
+
+
+newBinaryHeap = Heap(5)
+print(sizeofHeap(newBinaryHeap))
